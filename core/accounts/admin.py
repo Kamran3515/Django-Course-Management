7b10b75b05dd59e2d97ed644937e248903da2cd7
KAMRAN <kamranrezaeii.01@gmail.com>
2025-10-07 21:31:37 +0330

    edit postgres database

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,5 @@
 from django.utils.html import format_html
 from django.urls import reverse
-
 from django.contrib import admin
 from .models import *
 
@@ -10,10 +9,11 @@ class AdminUser(admin.ModelAdmin):
     list_display = ("id","email", "is_staff", "is_active","role","created_at",'view_profile_link')
 
     def view_profile_link(self, obj):
+        print(obj)
         try:
             profile = Profile.objects.get(user=obj)
             url = reverse('admin:accounts_profile_change', args=[profile.id])
-            return format_html('<a href="{}">{}</a>', url, obj.email)
+            return format_html('<a href="{}">{}</a>', url, profile.first_name)
         except Profile.DoesNotExist:
             return "No profile"
 
@@ -23,5 +23,11 @@ class AdminUser(admin.ModelAdmin):
 
 @admin.register(Profile)
 class AdminProfile(admin.ModelAdmin):
-    list_display = ["id", "user","first_name", "last_name", "bio", "created_at"]
-    list_display_links = ("user",)
+    list_display = ("id","user", "first_name", "last_name", "bio", "created_at", "view_user_link")
+
+    def view_user_link(self, obj):
+        user = obj.user
+        url = reverse('admin:accounts_user_change', args=[user.id])
+        return format_html('<a href="{}">{}</a>', url, user.email)
+
+    view_user_link.short_description = "User Email"
